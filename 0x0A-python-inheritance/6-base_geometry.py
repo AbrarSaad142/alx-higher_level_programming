cdc84c5f4af332c4034a5c8d2ca29341d1c3405c
Abrar Saad <abrarsaad3572022@gmail.com>
2024-02-05 11:47:33 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module with class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class with public function area"""
+    def area(self):
+        raise Exception("area() is not implemented")

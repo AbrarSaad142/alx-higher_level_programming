d8e9f31410d6171d32d0cd33c62a360d985546d9
Abrar Saad <abrarsaad3572022@gmail.com>
2024-04-11 23:13:17 +0200

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+MY_HOST = 'localhost'
+
+if __name__ == "__main__":
+    db = MySQLdb.Connect(host=MY_HOST,port=3306,user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()

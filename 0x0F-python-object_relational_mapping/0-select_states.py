dbdcaefd06da1559637b2f5de3c1fae674e936e8
Abrar Saad <abrarsaad3572022@gmail.com>
2024-04-12 00:39:47 +0200

    14. Cities in state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
+""" Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+from sys import argv
 
-MY_HOST = 'localhost'
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    db = MySQLdb.Connect(host=MY_HOST, port=3306, user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states")
+
     rows = cur.fetchall()
     for i in rows:
         print(i)

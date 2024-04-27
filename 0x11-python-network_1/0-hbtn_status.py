211fa3743854081239f187096fcba18b39d1676f
Abrar Saad <abrarsaad3572022@gmail.com>
2024-04-27 03:07:02 +0300

    4. What's my status? #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,9 @@
 import urllib.request
 
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))

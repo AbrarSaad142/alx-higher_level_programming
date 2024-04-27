04ce470b27f29f44856ee74b5916a1eb6625dfef
Abrar Saad <abrarsaad3572022@gmail.com>
2024-04-27 03:33:07 +0300

    5. Response header value #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,11 @@
 """script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
 """
-from urllib import request, error
-import sys
 
 
 if __name__ == "__main__":
-
+    from urllib import request, error
+    import sys
     try:
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode("UTF-8"))
